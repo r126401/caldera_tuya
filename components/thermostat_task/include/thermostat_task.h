9ba8cbde42db175ff8ed859666504c9dec583f01
jajica <r126401@gmail.com>
2025-12-01 13:57:11 +0100

    Se añade el componente de la tare thermostat_task

---
+++ components/thermostat_task/include/thermostat_task.h
@@ -0,0 +1,29 @@
+void create_task_thermostat();
+
+typedef enum STATUS_RELAY {
+    INDETERMINADO = -1,
+    OFF = 0,
+    ON = 1
+}STATUS_RELAY;
+
+typedef enum THERMOSTAT_ACTION {
+	NO_TOGGLE_THERMOSTAT,
+	TOGGLE_THERMOSTAT,
+   NO_ACTION_THERMOSTAT
+}THERMOSTAT_ACTION;
+
+typedef enum DEVICE_STATUS {
+
+   DEVICE_OK,
+   SENSOR_FAIL
+} DEVICE_STATUS;
+
+typedef enum THERMOSTAT_MODE {
+
+    MODE_AUTO,
+    MODE_AUTOMAN,
+    MODE_MANUAL,
+    MODE_ERROR
+
+} THERMOSTAT_MODE;
+
---
+++ components/thermostat_task/thermostat_task.c
@@ -0,0 +1,350 @@
+
+#include "thermostat_task.h"
+#include "esp_err.h"
+#include "stdbool.h"
+
+
+#include "onewire_bus.h"
+#include "onewire_types.h"
+#include "ds18b20.h"
+#include "driver/gpio.h"
+#include "tal_thread.h"
+#include "tal_log.h"
+#include <math.h>
+#include "esp_rom_sys.h"
+#include "esp_log.h"
+
+
+#define ONEWIRE_MAX_DS18B20 1
+#define CONFIG_SENSOR_THERMOSTAT_GPIO 17
+#define CONFIG_RELAY_GPIO 10
+static const char *TAG = "thermostat_task";
+//TaskHandle_t thermostat_handle = NULL;
+ds18b20_device_handle_t ds18b20s[ONEWIRE_MAX_DS18B20];
+
+
+
+static void init_ds18b20()
+{
+
+    
+
+    // install 1-wire bus
+    onewire_bus_handle_t bus = NULL;
+    onewire_bus_config_t bus_config = {
+        .bus_gpio_num = CONFIG_SENSOR_THERMOSTAT_GPIO,
+		.flags = {
+            .en_pull_up = true, // enable the internal pull-up resistor in case the external device didn't have one
+        }
+    };
+    onewire_bus_rmt_config_t rmt_config = {
+        .max_rx_bytes = 10, // 1byte ROM command + 8byte ROM number + 1byte device command
+    };
+    ESP_ERROR_CHECK(onewire_new_bus_rmt(&bus_config, &rmt_config, &bus));
+
+    int ds18b20_device_num = 0;
+    //ds18b20_device_handle_t ds18b20s[ONEWIRE_MAX_DS18B20];
+    onewire_device_iter_handle_t iter = NULL;
+    onewire_device_t next_onewire_device;
+    esp_err_t search_result = ESP_OK;
+
+    // create 1-wire device iterator, which is used for device search
+    ESP_ERROR_CHECK(onewire_new_device_iter(bus, &iter));
+    ESP_LOGI(TAG, "Device iterator created, start searching...");
+    do {
+        search_result = onewire_device_iter_get_next(iter, &next_onewire_device);
+        if (search_result == ESP_OK) { // found a new device, let's check if we can upgrade it to a DS18B20
+            ds18b20_config_t ds_cfg = {};
+			onewire_device_address_t address;
+            // check if the device is a DS18B20, if so, return the ds18b20 handle
+
+            if (ds18b20_new_device_from_enumeration(&next_onewire_device, &ds_cfg, &ds18b20s[ds18b20_device_num]) == ESP_OK) {
+				ds18b20_get_device_address(ds18b20s[ds18b20_device_num], &address);
+                ESP_LOGI(TAG, "Found a DS18B20[%d], address: %016llX", ds18b20_device_num, next_onewire_device.address);
+                ds18b20_device_num++;
+            } else {
+                ESP_LOGI(TAG, "Found an unknown device, address: %016llX", next_onewire_device.address);
+            }
+        }
+    } while (search_result != ESP_ERR_NOT_FOUND);
+    ESP_ERROR_CHECK(onewire_del_device_iter(iter));
+    ESP_LOGI(TAG, "Searching done, %d DS18B20 device(s) found", ds18b20_device_num);
+
+    // Now you have the DS18B20 sensor handle, you can use it to read the temperature
+
+}
+
+
+THERMOSTAT_MODE get_thermostat_mode() {
+
+    return MODE_AUTO;
+}
+
+
+enum STATUS_RELAY relay_operation(STATUS_RELAY op) {
+
+    return gpio_get_level(CONFIG_SENSOR_THERMOSTAT_GPIO);
+}
+
+
+
+static enum THERMOSTAT_ACTION calcular_accion_termostato(STATUS_RELAY *accion, float current_temperature) {
+
+	float margin_temperature = 0.5;
+	float threshold_temperature = 21.5; // Es el umbral de temperatura para calcular la accion del termostato.
+
+
+    
+    
+    //set_lcd_update_threshold_temperature(threshold_temperature);
+	ESP_LOGI(TAG, "THRESHOLD : %.1f", threshold_temperature);
+	
+
+    if (gpio_get_level(CONFIG_RELAY_GPIO) == OFF) {
+               if (current_temperature <= (threshold_temperature - margin_temperature)) {
+            	   ESP_LOGI(TAG, "RELE OFF Y SE ENCIENDE. tempMedida: %.2f, tempUmbral: %.02f", current_temperature, threshold_temperature);
+                   *accion = ON;
+                   return TOGGLE_THERMOSTAT;
+               } else {
+            	   ESP_LOGI(TAG, "RELE OFF Y DEBE SEGUIR SIGUE OFF. tempMedida: %.2f, tempUmbral: %.02f", current_temperature, threshold_temperature);
+                   *accion = OFF;
+                   return NO_TOGGLE_THERMOSTAT;
+
+               }
+           } else {
+               if (current_temperature >= (threshold_temperature + margin_temperature) ) {
+            	   ESP_LOGI(TAG, "RELE ON Y SE APAGA. tempMedida: %.2f, tempUmbral: %.02f", current_temperature, threshold_temperature);
+                   *accion = OFF;
+                   return TOGGLE_THERMOSTAT;
+               } else {
+            	   ESP_LOGI(TAG, "RELE ON Y DEBE SEGUIR SIGUE ON. tempMedida: %.2f, tempUmbral: %.02f", current_temperature, threshold_temperature);
+                   *accion = ON;
+                   return NO_TOGGLE_THERMOSTAT;
+
+               }
+           }
+
+		
+
+}
+
+
+
+THERMOSTAT_ACTION thermostat_action(float current_temperature) {
+
+	enum STATUS_RELAY accion_rele;
+	enum THERMOSTAT_ACTION accion_termostato;
+	
+	float temperature = -1;
+
+	ESP_LOGI(TAG, "Estamos en thermostat_action");
+
+    if ((get_thermostat_mode() == MODE_MANUAL) || (get_thermostat_mode() == MODE_ERROR)) {
+        ESP_LOGW(TAG, "No se hace nada ya que tenemos el termostato en modo manual");
+        return NO_ACTION_THERMOSTAT;
+
+    }
+
+	ESP_LOGI(TAG, "accionar_termostato: LECTURA ANTERIOR: %.2f, LECTURA POSTERIOR: %.2f HA HABIDO CAMBIO DE TEMPERATURA", 
+			temperature, current_temperature);
+
+    if (((accion_termostato = calcular_accion_termostato(&accion_rele, current_temperature)) == TOGGLE_THERMOSTAT)) {
+    	ESP_LOGI(TAG, "VAMOS A ACCIONAR EL RELE");
+    	//*******hacer relay_operation(datosApp, TEMPORIZADA, accion_rele);
+		relay_operation(accion_rele);
+    } else {
+		accion_termostato = NO_TOGGLE_THERMOSTAT;
+    }
+
+    
+	return accion_termostato;
+}
+
+
+
+#define DEFAULT_THRESHOLD 21.0
+
+
+
+
+static float redondear_temperatura(float temperatura) {
+
+	float redondeado;
+	float diferencia;
+	float resultado = 0;
+	float valor_absoluto;
+
+	redondeado = lround(temperatura);
+	diferencia = temperatura - redondeado;
+	//ESP_LOGE(TAG, "temperatura: %.2f, redondeado: %.2f, diferencia: %.2f", temperatura, redondeado, diferencia);
+	if (diferencia == 0) {
+		resultado = temperatura;
+		//ESP_LOGI(TAG, ""TRAZAR"TEMPERATURA ORIGINAL: %.2f, TEMPERATURA REDONDEADA: %.2f,", INFOTRAZA, temperatura, resultado);
+		return resultado;
+
+	}
+	valor_absoluto = fabs(redondeado);
+	//ESP_LOGE(TAG, "temperatura: %.2f, redondeado: %.2f, diferencia: %.2f, valor absoluto :%.2f", temperatura, redondeado, diferencia, valor_absoluto);
+	if (diferencia <= 0.25) {
+		ESP_LOGI(TAG, "diferencia <= 0.25");
+		resultado = valor_absoluto;
+	}
+	if ((diferencia > 0.25 ) && (diferencia < 0.5)) {
+		ESP_LOGI(TAG, "((diferencia > 0.25 ) && (diferencia < 0.5))");
+		resultado = valor_absoluto + 0.5;
+	}
+
+	if ((diferencia < -0.25)) {
+		ESP_LOGI(TAG, "diferencia < -0.25");
+		resultado = valor_absoluto - 0.5;
+	}
+
+	ESP_LOGI(TAG, "TEMPERATURA ORIGINAL: %.2f, TEMPERATURA REDONDEADA: %.2f,", temperatura, resultado);
+	return resultado;
+
+}
+
+
+
+
+
+
+
+static esp_err_t read_temperature(float *temperature_metered) 
+
+{
+	esp_err_t error; 
+
+	if (ds18b20_trigger_temperature_conversion(*ds18b20s) != ESP_OK) {
+		ESP_LOGE(TAG, "Error al hacer la conversion de temperatura");
+		return ESP_FAIL;
+	}
+	if ((error = ds18b20_get_temperature(*ds18b20s, temperature_metered)) == ESP_OK) {
+
+        ESP_LOGI(TAG, "Alarma de sensor off");
+		//set_alarm(SENSOR_ALARM, ALARM_APP_OFF);
+
+	} else {
+
+        ESP_LOGE(TAG, "Alarma de sensor on");
+
+		//set_alarm(SENSOR_ALARM, ALARM_APP_ON);
+	}
+
+	
+
+    return error;
+    
+
+}
+
+
+
+
+esp_err_t reading_local_temperature(float *current_temperature) {
+
+    esp_err_t error = ESP_FAIL;
+	float temperatura_a_redondear;
+	//float current_temperature;
+	float calibrate = -3.5;
+
+    ESP_LOGI(TAG, "Leyendo desde el sensor. Calibrado: %.2f", calibrate);
+
+	error = read_temperature(current_temperature);
+	if (error == ESP_OK) {
+		ESP_LOGI(TAG," Lectura local correcta!. ");
+		temperatura_a_redondear = *current_temperature + calibrate;
+		*current_temperature = redondear_temperatura(temperatura_a_redondear);
+		thermostat_action(*current_temperature);
+		ESP_LOGI(TAG, "Actualizada y enviada la temperatura actualizada: %.2f", *current_temperature);
+	} else {
+		ESP_LOGE(TAG, " Error al leer desde el sensor de temperatura");
+	}
+
+	
+
+
+	return error;
+}
+
+/* Tuya thread handle */
+static THREAD_HANDLE ty_iotThermostat = NULL;
+
+static void task_iotThermostat() 
+{
+
+	esp_err_t error = ESP_OK ;
+	int value;
+	char* id_sensor;
+	static uint8_t n_errors = 0;
+	float current_temperature;
+	//event_lcd_t event;
+	//event.event_type = UPDATE_TEMPERATURE;
+
+    ESP_LOGI(TAG, "Ya estamos ejecutando la task del termostato");
+
+
+	/**
+	 * init driver ds18b20
+	 */
+
+	relay_operation(false);
+
+	
+	init_ds18b20();
+
+    while (1) {
+
+        		
+
+        ESP_LOGI(TAG, " Leemos temperatura en local");
+        error = reading_local_temperature(&current_temperature);
+        if (error == ESP_OK) {
+
+            //set_lcd_update_temperature(current_temperature);
+            ESP_LOGI(TAG, "Enviada la temperatura al display");
+             esp_rom_delay_us(30000 * 1000);
+           
+            
+
+        }
+
+			
+
+	
+    }
+   
+	
+
+
+    tal_thread_delete(ty_iotThermostat);
+    ty_iotThermostat = NULL;
+
+	
+}
+
+
+
+
+
+void create_task_thermostat() {
+
+     //xTaskCreatePinnedToCore(task_iotThermostat, "tarea_lectura_temperatura", 2048, (void*) NULL, 4, NULL,0);
+     //ESP_LOGW(TAG, "Tarea thermostat task creada");
+
+    ESP_LOGE(TAG, "ERROR: No se pudo crear el thread task_iotThermostat()");
+    THREAD_CFG_T thrd_param = {2048, 4, "task_iotThermostat"};
+    tal_thread_create_and_start(&ty_iotThermostat, NULL, NULL, task_iotThermostat, NULL, &thrd_param);
+    if (!ty_iotThermostat) {
+    ESP_LOGE(TAG, "ERROR: No se pudo crear el thread tuya_app_main()");
+}
+else {
+    ESP_LOGI(TAG, "Thread ty_iotThermostat creado OK");
+}
+
+
+
+
+}
+
+
